ab25d225204fc56cd090f93e860bea6687fca7b2
devayesha19 <ayesha.zargham@websays.com>
2024-09-03 13:35:34 +0500

    facebook

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ redisClient.sadd("fb_proxy_users", ["carolynkingnerg,&1@l5P#pUQ71,http://8oqlg3E
 })
 let browsers = [];
 async function launchBrowser(proxyUrl, user) {
-    let newproxy = await proxyChain.anonymizeProxy(proxyUrl)
-    let browser = await puppeteer.launch({
+  let newproxy = await proxyChain.anonymizeProxy(proxyUrl);
+  let browser = await puppeteer.launch({
     headless: true,
     args: [
       `--proxy-server=${newproxy}`,
@@ -27,53 +27,53 @@ async function launchBrowser(proxyUrl, user) {
       '--ignore-certifcate-errors',
       '--ignore-certifcate-errors-spki-list',
       '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.45 Safari/537.36'
-    	],
-      userDataDir: `./cache/user_${user}`
-  	});
-    const browserData = { browser: browser, user: user, proxy: proxyUrl};
-    browsers.push(browserData)
-    return browser
+    ],
+    userDataDir: `./cache/user_${user}`
+  });
+  browsers.push({ browser: browser, user: user, proxy: proxyUrl });
+  return browser;
 }
 async function scrollToEnd(page) {
   await page.evaluate(async () => {
     await new Promise((resolve) => {
       const maxScrollAttempts = 10;
       let scrollAttempt = 0;
+
       function scroll() {
         window.scrollTo(0, document.body.scrollHeight);
         scrollAttempt++;
+
         if (scrollAttempt < maxScrollAttempts) {
           requestAnimationFrame(scroll);
         } else {
           resolve();
         }
       }
+
       scroll();
     });
   });
 }
 async function processLink(browser, postUrl, dateKey, dateValue, ogAuthorImageContent, ogAuthorDescription, authorUrl, profileId, postTitle) {
-  const page = await browser.newPage()
+  const page = await browser.newPage();
   try {
     await page.goto(postUrl);
   } catch (error) {
-    return false
+    return false;
   }
   await new Promise(r => setTimeout(r, 30000));
+
   const htmlContent = await page.content();
-const $ = cheerio.load(htmlContent);
-  const image = $("meta[property='og:image']");
-  const ogImageContent = image.attr('content');
-  const ogTitle = $("meta[property='og:title']");
-  const ogTitleContent = ogTitle.attr('content');
-  const ogDescription = $("meta[name='description']");
-  const ogDescriptionContent = ogDescription.attr('content');
-  const firstTimeTag = $('time').first();
- const datetime = firstTimeTag.attr('datetime');
-  const title = firstTimeTag.attr('title');
-  const content = firstTimeTag.text();
-  const dateObject = new Date(datetime);
-  const unixTimestamp = Math.floor(dateObject.getTime() / 1000);
+  const $ = cheerio.load(htmlContent);
+
+  // Modify selectors based on Facebook's structure
+  const ogImageContent = $("meta[property='og:image']").attr('content');
+  const ogTitleContent = $("meta[property='og:title']").attr('content');
+  const ogDescriptionContent = $("meta[property='og:description']").attr('content');
+
+  const datetime = $('abbr').first().attr('data-utime');
+  const unixTimestamp = Math.floor(datetime);
+
   let jsonData = {
     title: ogTitleContent,
     description: ogDescriptionContent,
@@ -86,17 +86,21 @@ const $ = cheerio.load(htmlContent);
     postUrl: postUrl,
     postTitle: ogTitleContent
   };
-  const apiUrl = 'https://wapi.websays.com/winput/ig/spider/clippings';
-  page.close()
-  if (dateValue!=null) {
-    if(unixTimestamp > dateValue) {
-      let redisTimeStamp = await redisClient.getKey(dateKey)
+
+  const apiUrl = 'https://wapi.websays.com/winput/fb/spider/clippings';
+  page.close();
+
+  if (dateValue != null) {
+    if (unixTimestamp > dateValue) {
+      let redisTimeStamp = await redisClient.getKey(dateKey);
       let redisDateValue = parseInt(redisTimeStamp, 10);
+
       if (!redisTimeStamp) {
-        redisDateValue = 0
+        redisDateValue = 0;
       }
+
       if (unixTimestamp > redisDateValue) {
-        await redisClient.setKey(dateKey, unixTimestamp)
+        await redisClient.setKey(dateKey, unixTimestamp);
       }
       try {
         const response = await axios.post(apiUrl, jsonData);
@@ -105,132 +109,133 @@ const $ = cheerio.load(htmlContent);
         if (error.response) {
           console.error('Response Data:', error.response.data);
         }
-        return false
+        return false;
       }
+
     } else {
-      return false
+      return false;
     }
   } else {
-    await redisClient.setKey(dateKey, unixTimestamp)
+    await redisClient.setKey(dateKey, unixTimestamp);
     try {
       const response = await axios.post(apiUrl, jsonData);
     } catch (error) {
       console.error('Error:', error.response.data || error.message);
-      return false
+      return false;
     }
   }
-  return true
+  return true;
 }
-async function login(user, password, browser) { 
+async function login(user, password, browser) {
+  const page = await browser.newPage();
   try {
-    const page = await browser.newPage();
-    await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher');
+    await page.goto('https://www.facebook.com/login');
     await new Promise(r => setTimeout(r, 5000));
-    const dialogDiv = await page.$('div[role="dialog"]');
-    if (dialogDiv) {
-        const buttonsInsideDialog = await dialogDiv.$$('button');
-        const secondToLastButtonIndex = buttonsInsideDialog.length - 2;
-        if (secondToLastButtonIndex >= 0) {
-          await buttonsInsideDialog[secondToLastButtonIndex].click();
-          await new Promise(r => setTimeout(r, 10000));
-        } else {
-        }
-    } else {
-    }
-    await new Promise(r => setTimeout(r, 2000));
-    await page.type('input[name="username"]', user);
-    await page.type('input[name="password"]', password);
-    await page.click('button[type="submit"]');
+
+    await page.type('input[name="email"]', user);
+    await page.type('input[name="pass"]', password);
+
+    await page.click('button[name="login"]');
     await new Promise(r => setTimeout(r, 10000));
-  } catch (error) {    
+  } catch (error) {
+    console.log("Error during login:", error);
   }
 }
+
 function randomlyRotateArray(arr) {
   const randomIndex = Math.floor(Math.random() * arr.length);
   const rotatedArray = arr.slice(randomIndex).concat(arr.slice(0, randomIndex));
   return rotatedArray;
 }
+
 async function main() {
-  const mainLink = "https://www.instagram.com/"
-  var browserCount = 0
-  redisClient.smembers("ig_channels", async (err, values) => {
+  const mainLink = "https://www.facebook.com/";
+
+  var browserCount = 0;
+  redisClient.smembers("fb_channels", async (err, values) => {
     values = randomlyRotateArray(values);
-    for(var i=0; i<values.length; i++) {
-      let browser = browsers[browserCount].browser
-      browserCount++
+    for (var i = 0; i < values.length; i++) {
+      let browser = browsers[browserCount].browser;
+      browserCount++;
       if (browserCount == browsers.length) {
-        browserCount = 0
+        browserCount = 0;
       }
-      let value = values[i]
-      channelsArray = value.split(':')
-      channelId = channelsArray[0]
-      url = mainLink + channelsArray[1]
+
+      let value = values[i];
+      channelsArray = value.split(':');
+      channelId = channelsArray[0];
+      url = mainLink + channelsArray[1];
       let newPage = await browser.newPage();
       try {
-        await newPage.goto(url)
+        await newPage.goto(url);
       } catch (error) {
-        continue
+        continue;
       }
       await new Promise(r => setTimeout(r, 10000));
+
       htmlContent = await newPage.content();
       $ = cheerio.load(htmlContent);
-      newPage.close()
-      const authorImage = $("meta[property='og:image']");
-      const ogAuthorImageContent = authorImage.attr('content');
-      const authorDescription = $("meta[property='og:description']");
-      const ogAuthorDescription = authorDescription.attr('content');
-      const postTitle = $("meta[property='og:description']");
-      const postTitleDescription = authorDescription.attr('content');
-      const article = $('article');
-      const children = article.children();
-      dateKey = channelId+"_"+url+"_lasttime"
-      let data = await redisClient.getKey(dateKey)
-      const child = $(children[0]);
-      const grandchildren = child.find('a');
-      var start = 0
-      var end = grandchildren.length
+
+      newPage.close();
+      const ogAuthorImageContent = $("meta[property='og:image']").attr('content');
+      const ogAuthorDescription = $("meta[property='og:description']").attr('content');
+      const postTitle = $("meta[property='og:description']").attr('content');
+      const article = $('div[data-pagelet="MainFeed"]'); // Adjust based on actual Facebook structure
+
+      dateKey = channelId + "_" + url + "_lasttime";
+      let data = await redisClient.getKey(dateKey);
+
+      const children = article.find('div[data-ad-preview="message"]'); // Adjust based on actual Facebook structure
+
+      var start = 0;
+      var end = children.length;
       if (data == null) {
-        start = grandchildren.length - 1
-        end = 0
+        start = children.length - 1;
+        end = 0;
       }
-      for (var j = start;; ) {
+
+      for (var j = start;;) {
         let dateValue = 0;
         if (data == null) {
-          if (j<=end) {
-            break
+          if (j <= end) {
+            break;
           }
         } else {
           dateValue = parseInt(data, 10);
-          if (j>end) {
-            break
-          }
-        }
-          var grandchildElement = grandchildren[j]
-          const link = $(grandchildElement);
-          const href = link.attr('href');
-          const text = link.text();
-          var postUrl = mainLink + href
-          var forward = await processLink(browser, postUrl, dateKey, dateValue, ogAuthorImageContent, ogAuthorDescription, url, channelId, postTitle)
-          if (!forward && j >= 3) { 
+          if (j > end) {
             break;
           }
-          if (data == null) {
-            j--
-          } else {
-            j++
-          }
         }
+        var child = children[j];
+        const postUrl = mainLink + $(child).find('a').attr('href');
+
+        var forward = await processLink(browser, postUrl, dateKey, dateValue, ogAuthorImageContent, ogAuthorDescription, url, channelId, postTitle);
+        if (!forward && j >= 3) {
+          break;
+        }
+
+        if (data == null) {
+          j--;
+        } else {
+          j++;
+        }
+      }
     }
-  })  
-  await new Promise(r => setTimeout(r, 24* 60 * 60 *1000));  
+  });
+
+  await new Promise(r => setTimeout(r, 24 * 60 * 60 * 1000));
 }
+
 const loop = async () => {
-  var users = await redisClient.smembersAsync("ig_proxy_users")
-  for (var j=0; j<users.length; j++) {
+  var users = await redisClient.smembersAsync("fb_proxy_users");
+
+  for (var j = 0; j < users.length; j++) {
     const dataArray = users[j].split(',');
-    browser = await launchBrowser(dataArray[2], dataArray[0])
-    await login(dataArray[0], dataArray[1], browser)
+    browser = await launchBrowser(dataArray[2], dataArray[0]);
+    await login(dataArray[0], dataArray[1], browser);
   }
-  for (;;) await main()
-}
-loop()
+
+  for (;;) await main();
+};
+
+loop();
